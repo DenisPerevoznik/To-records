9cf4f5eaa78ac210c322debfdb1a367f3eb68472
DenisPerevoznik <den.perevoznik@gmail.com>
2020-08-18 01:00:03 +0300

    initial

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const config = require('config');
 const mongo = require('mongoose');
 const colors = require("colors");
-
+const path = require('path');
 const app = express();
 const PORT = config.get('port') || 5000;
 
@@ -13,19 +13,20 @@ app.use('/api/notepad', require('./routes/notepad.routes'));
 app.use('/api/notepad', require('./routes/record.routes'));
 app.use('/api/profile', require('./routes/profile.routes'));
 
+if(process.env.NODE_ENV === "production"){
+    app.use('/', express.static(path.join(__dirname, "client", "build")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 mongo.connect(config.get('mongoUri'));
 
 // mongo.connection.on("connected", ()=>{
 //     console.log(colors.green("Подключение к базе данных установлено =)"));
     
 // });
-
-if(config.get("isProduction")){
-    app.use(express.static(path.join(__dirname, '/client/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/build'))
-    })
-}
 
 mongo.connection.on('error', (error) => {
 
