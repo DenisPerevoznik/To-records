d6f7c86718c15946170e9bb860aadf69b9f1406a
DenisPerevoznik <den.perevoznik@gmail.com>
2020-08-17 23:57:31 +0300

    to records

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ mongo.connect(config.get('mongoUri'));
 // });
 
 if(config.get("isProduction")){
-    app.use(express.static(path.join(__dirname, './client/build')))
+    app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build'))
+        res.sendFile(path.join(__dirname, '/client/build'))
     })
 }
 
